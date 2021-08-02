223c579b5108dd15a809b776f1c8cb66c0cc2270
eriklenaerts <erik.lenaerts@line20.be>
2021-08-02 14:03:01 +0200

    support for blanks in resource names

--- src/resource.js
+++ src/resource.js
@@ -18,16 +18,19 @@ export default class resource {
     constructor(resourceString, verbose) {
         if (resourceString) {
             // test this regex here: https://regex101.com/r/ZLQZGW/1
-            let match = (/^(?:(?<parent>[a-zA-Z0-9-]+)\/{1}){0,2}(?<resource>[a-zA-Z0-9-]+)?(?:\[(?<ops>[2-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-4])\]+)?(?:::(?<tag>[a-zA-Z0-9-]+))?$/g).exec(resourceString);
+            let match = (/^(?:(?<parent>[a-zA-Z0-9-\s]+)\/{1}){0,2}(?<resource>[a-zA-Z0-9-\s]+)?(?:\[(?<ops>[2-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-4])\]+)?(?:::(?<tag>[a-zA-Z0-9-]+))?$/g).exec(resourceString);
             if (!match)
                 throw new Error(`There\'s something wrong with the format of this (${resourceString}) resource argument`);
 
+            let saveName = match.groups.resource.replace(/[^a-z0-9_]+/gi, '-').replace(/^-|-$/g, '').toLowerCase();
+            let parameterName = match.groups.resource.replace(/\W+(.)/g, (match, chr) => { return chr.toUpperCase();}) + 'Id';
+
             this.verbose = verbose
-            this.name = pluralize.singular(match.groups.resource);
+            this.name = pluralize.singular(saveName);
             this.tag = match.groups.tag || match.groups.resource;
-            this.collection = pluralize(match.groups.resource);
+            this.collection = pluralize(saveName);
             this.parent = match.groups.parent ? new resource(match.groups.parent) : null;
-            this.idParameter = this.name + 'Id';
+            this.idParameter = parameterName;
             this.ops = new operations(match.groups.ops || defaultOpsModifier, this.verbose);
             this.collectionPath = this.ops.hasCollectionOps ? this.determinePath(this.parent, this.collection) : null;
             this.resourcePath = this.ops.hasResourceOps ? this.determinePath(this.parent, this.collection, this.idParameter) : null;
